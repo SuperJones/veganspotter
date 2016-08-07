641595d739a54f724b4aaa57024a53ded7cfa853
Anissa Jones <anissajones10@gmail.com>
2016-08-06 21:22:50 -0400

    Adds restful routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,56 +1,28 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'main#index'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  resources :restaurants do
+    resources :foods
+  end
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+#   Prefix Verb   URI Pattern                                          Controller#Action
+#                 root GET    /                                                    main#index
+#     restaurant_foods GET    /restaurants/:restaurant_id/foods(.:format)          foods#index
+#                      POST   /restaurants/:restaurant_id/foods(.:format)          foods#create
+#  new_restaurant_food GET    /restaurants/:restaurant_id/foods/new(.:format)      foods#new
+# edit_restaurant_food GET    /restaurants/:restaurant_id/foods/:id/edit(.:format) foods#edit
+#      restaurant_food GET    /restaurants/:restaurant_id/foods/:id(.:format)      foods#show
+#                      PATCH  /restaurants/:restaurant_id/foods/:id(.:format)      foods#update
+#                      PUT    /restaurants/:restaurant_id/foods/:id(.:format)      foods#update
+#                      DELETE /restaurants/:restaurant_id/foods/:id(.:format)      foods#destroy
+#          restaurants GET    /restaurants(.:format)                               restaurants#index
+#                      POST   /restaurants(.:format)                               restaurants#create
+#       new_restaurant GET    /restaurants/new(.:format)                           restaurants#new
+#      edit_restaurant GET    /restaurants/:id/edit(.:format)                      restaurants#edit
+#           restaurant GET    /restaurants/:id(.:format)                           restaurants#show
+#                      PATCH  /restaurants/:id(.:format)                           restaurants#update
+#                      PUT    /restaurants/:id(.:format)                           restaurants#update
+#                      DELETE /restaurants/:id(.:format)                           restaurants#destroy
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
