27647f38fabe7b586fcce72a44f1712e678e0d45
Anissa Jones <anissajones10@gmail.com>
2016-08-09 18:57:19 -0400

    Adds some controller information from previous project.

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,3 +1,20 @@
 class FoodsController < ApplicationController
 
+  def index
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @foods = @restaurant.foods
+    # render status: 200, json: @foods.to_json
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @foods = @restaurant.foods
+    # render status: 200, json: @foods.to_json
+  end
+
+  def all
+    @foods = Food.all
+    # render status: 200, json: @foods.to_json
+  end
+
 end
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,29 @@
 class RestaurantsController < ApplicationController
   def index
-    
+    @restaurants = Restaurant.all
   end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def create
+    @restaurant = Restaurant.create!(restaurant_params)
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update!(restaurant_params)
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+  end
+
+  private
+  def restaurant_params
+    params.permit(:name, :info, :category, :photo_url, :gotchas)
+  end
+
 end
